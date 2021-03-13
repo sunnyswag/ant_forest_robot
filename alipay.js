5e8c5918385e9af7b5d4513b1d4983cf04f4bf9e
huiqing <hhq126152@gmail.com>
2021-03-13 19:29:59 +0800

    finishi_part_one

--- alipay.js
+++ alipay.js
@@ -2,7 +2,7 @@ var screen_width = device.width;
 var screen_height = device.height;
 setScreenMetrics(screen_width, screen_height); // 适配其他分辨率的手机
 
-// mainFunction();
+mainFunction();
 
 // 1080 x 2400
 // 坐标在左上角
@@ -11,10 +11,20 @@ setScreenMetrics(screen_width, screen_height); // 适配其他分辨率的手机
 
 function mainFunction(){
     unlock(); // 解锁
+    // init();
     openAlipay();
     enterAntForest();
     collectOthers(); // TODO
     getAlipyPoints(); // TODO
+}
+
+function init(){
+    // 需要手动点击弹窗的确认按钮
+    if(!requestScreenCapture()){
+        toast("请求截图失败！");
+        exit();
+    }
+    sleep(500);
 }
 
 function unlock(){
@@ -43,6 +53,7 @@ function openAlipay(){
 function enterAntForest(){
     click("蚂蚁森林");
     sleep(4000);
+    click("140", "645"); // 关闭弹窗
 
     // 收集自己的能量
     collectEnergy();
@@ -52,17 +63,22 @@ function enterAntForest(){
 }
 
 function collectOthers(){
-    # TODO 
-    click(1000, 1580);
-    sleep(2000);
-    collectEnergy();
-    sleep(2000);
+    while(!click("能量收完啦")){
+        click(1000, 1580);
+        sleep(2000);
+        collectEnergy();
+        sleep(500);
+    }
+    click("返回我的森林");
+    toast("能量收集完毕");
+    exit();
+
 }
 
 function collectEnergy(){
-    // height：480 ~ 950
+    // height：570 ~ 950
     // width ：130 ~ 960
-    for(var row = screen_height * 0.2; row < screen_height * 0.396; row += 80)
+    for(var row = screen_height * 0.238; row < screen_height * 0.396; row += 80)
         for(var column = screen_width * 0.12; column < screen_width * 0.889; column += 80)
             click(column, row);
 }
